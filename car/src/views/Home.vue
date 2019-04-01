03d07e44654430ecb66b398c8fc4a19e05e08aa4
heyunqiang <heyunqiang99@163.com>
2019-04-01 22:03:18 +0800

    car二手车

--- car/src/views/Home.vue
+++ car/src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <p>vuex-state-cont</p>
+      <p>{{ count }}</p>
+    </div>
   </div>
 </template>
 
@@ -8,10 +12,23 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
+import { mapState } from "vuex"
+
 export default {
   name: 'home',
   components: {
     HelloWorld
-  }
+  },
+  data() {
+    return {
+      
+    }
+  },
+  computed: {
+  localComputed () { /* ... */ },
+  ...mapState({
+    count
+  })
+}
 }
 </script>
--- vuex/README.md
+++
@@ -1,3 +0,0 @@
-# vuex 状态管理模式
- 
-[单向数据流](https://vuex.vuejs.org/flow.png)
